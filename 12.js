9c8a756ebbe86bdf286cbf972c37cf4f604c970c
Csandori <csandori@gmail.com>
2021-12-12 15:56:07 +0100

    Update 12.js

--- 12.js
+++ 12.js
@@ -15,29 +15,9 @@ let system = input.reduce((a, b) => {
   return a;
 }, {});
 
-let partOne = () => {
-  return partOneMain(["start"], []).length;
-  function partOneMain(route, routes) {
-    let position = route[route.length - 1];
-    system[position].forEach((direction) => {
-      if (
-        !(direction.toLowerCase() === direction && route.includes(direction)) &&
-        direction !== "start"
-      ) {
-        if (direction === "end") {
-          routes.push([...route, direction]);
-        } else {
-          partOneMain([...route, direction], routes);
-        }
-      }
-    });
-    return routes;
-  }
-};
-
-let partTwo = () => {
-  return partTwoMain(["start"], [], false).length;
-  function partTwoMain(route, routes, visitedTwice) {
+let task = (isItFirst) => {
+  return taskMain(["start"], [], isItFirst).length;
+  function taskMain(route, routes, visitedTwice) {
     let position = route[route.length - 1];
     system[position].forEach((direction) => {
       if (direction !== "start") {
@@ -48,17 +28,17 @@ let partTwo = () => {
           route.includes(direction) &&
           !visitedTwice
         ) {
-          partTwoMain([...route, direction], routes, true);
+          taskMain([...route, direction], routes, true);
         } else if (
           direction.toLowerCase() !== direction ||
           (direction.toLowerCase() === direction && !route.includes(direction))
         ) {
-          partTwoMain([...route, direction], routes, visitedTwice || false);
+          taskMain([...route, direction], routes, visitedTwice || false);
         }
       }
     });
     return routes;
   }
 };
-console.log(partOne());
-console.log(partTwo());
+console.log(task(true));
+console.log(task(false));
